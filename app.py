33076259d2a3aeaade3bf94dcf80947973c23dc5
Nitishk <patranitish267@gmail.com>
2025-05-17 12:58:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def hello():
     cur.execute('SELECT message FROM messages')
     messages = cur.fetchall()
     cur.close()
+    print("✔ / route hit, messages len =", len(messages))
     return render_template('index.html', messages=messages)
 
 @app.route('/submit', methods=['POST'])
